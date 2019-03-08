b061eb62f7d01632789196c668aefbbb367f22ec
Martin Køhne Henriksen <Martinkhenriksen@gmail.com>
2019-03-08 10:58:42 +0100

    Add files via upload

---
+++ su19-excercises/Galaga-Exercise-1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Galaga_Exercise_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.GameLoop();
+            
+        }
+    }
+}
